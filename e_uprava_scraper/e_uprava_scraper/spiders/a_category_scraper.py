0c554ce2d26eaf39997f68883b418baa1489812d
Nik-tech-cloud <62603376+Nik-tech-cloud@users.noreply.github.com>
2023-08-03 23:43:58 +0200

    Simple spider with logging. Compatible with the new fetch method

--- e_uprava_scraper/e_uprava_scraper/spiders/a_category_scraper.py
+++ e_uprava_scraper/e_uprava_scraper/spiders/a_category_scraper.py
@@ -7,38 +7,25 @@ KRANJ = 223
 class ACategoryScraperSpider(scrapy.Spider):
     name = "a_category"
     allowed_domains = ["e-uprava.gov.si"]
-    base_url = ""
-
-    def __init__(self, name=None, **kwargs):
-        self.location = kwargs.get('location')
-        assert self.location == "kranj" or self.location == "jesenice", "location parameter should be either kranj or jesenice"
-        if self.location == "kranj":
-            self.location = str(KRANJ)
-        if self.location == "jesenice":
-            self.location = str(JESENICE)
 
     def start_requests(self):
         curr_date = date.today()
-        # format date to "2023-08-03" string
         curr_date = curr_date.strftime("%Y-%m-%d")
-        self.base_url = f"https://e-uprava.gov.si/si/javne-evidence/prosti-termini/content/singleton.html?=&type=1&cat=4&izpitniCenter=18&lokacija={self.location}&calendar_date=2023-08-03&offset=0&sentinel_type=ok&sentinel_status=ok&is_ajax=1"
-        self.crawler.stats.set_value('pages_checked', 0)
-        yield scrapy.Request(url=self.base_url, callback=self.parse, meta={"playwright": True})
+        urls = [
+            f"https://e-uprava.gov.si/si/javne-evidence/prosti-termini/content/singleton.html?=&type=1&cat=4&izpitniCenter=18&lokacija={KRANJ}&calendar_date={curr_date}&offset=0&sentinel_type=ok&sentinel_status=ok&is_ajax=1",
+            f"https://e-uprava.gov.si/si/javne-evidence/prosti-termini/content/singleton.html?=&type=1&cat=4&izpitniCenter=18&lokacija={JESENICE}&calendar_date={curr_date}&offset=0&sentinel_type=ok&sentinel_status=ok&is_ajax=1"
+        ]
+        for url in urls:
+            yield scrapy.Request(url=self.url, callback=self.parse, meta={"playwright": True})
 
 
     def parse(self, response):
-        open_appointments = response.css('didv.js_dogodekBox.dogodek')
-        if len(open_appointments) == 0:
-            # no appointments found on this page, go to next page
-            self.crawler.stats.inc_value('pages_checked')
-            new_url = self.base_url + "&page=" + str(self.crawler.stats.get_value('pages_checked'))
-            yield scrapy.Request(url=new_url, callback=self.parse, meta={"playwright": True})
-        else:
-            # appointments found on this page
-            for appointment in open_appointments:
-                free_date = appointment.css(".sr-only::text").get().strip()
-                self.log(f"Free date: {free_date}")
-        
-        
-# new url
-# https://e-uprava.gov.si/si/javne-evidence/prosti-termini/content/singleton.html?=&type=1&cat=4&izpitniCenter=18&lokacija=223&calendar_date=2023-08-03&offset=0&sentinel_type=ok&sentinel_status=ok&is_ajax=1
+        open_appointments = response.css('div.js_dogodekBox.dogodek')
+        self.log(open_appointments)
+        for appointment in open_appointments:
+            free_date = appointment.css(".calendarBox > .sr-only::text").get()
+            location = appointment.css(".upperOpomnikDiv > span:nth-child(2)::text").get()
+            time = appointment.css(".contentOpomnik.celotnaSirina > div:nth-child(6) > span.bold::text").get()
+            self.log(f"Free date: {free_date}")
+            self.log(f"At location: {location}")
+            self.log(f"At time: {time}\n")
