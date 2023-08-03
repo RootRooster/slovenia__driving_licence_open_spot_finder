0dbd201eadf0ec284c09bc83fd9972d54f6fbf78
Nik-tech-cloud <62603376+Nik-tech-cloud@users.noreply.github.com>
2023-08-03 23:46:48 +0200

    Spider fixed

--- e_uprava_scraper/e_uprava_scraper/spiders/a_category_scraper.py
+++ e_uprava_scraper/e_uprava_scraper/spiders/a_category_scraper.py
@@ -4,6 +4,7 @@ from time import sleep
 JESENICE = 171
 KRANJ = 223
 
+
 class ACategoryScraperSpider(scrapy.Spider):
     name = "a_category"
     allowed_domains = ["e-uprava.gov.si"]
@@ -16,16 +17,17 @@ class ACategoryScraperSpider(scrapy.Spider):
             f"https://e-uprava.gov.si/si/javne-evidence/prosti-termini/content/singleton.html?=&type=1&cat=4&izpitniCenter=18&lokacija={JESENICE}&calendar_date={curr_date}&offset=0&sentinel_type=ok&sentinel_status=ok&is_ajax=1"
         ]
         for url in urls:
-            yield scrapy.Request(url=self.url, callback=self.parse, meta={"playwright": True})
-
+            yield scrapy.Request(url=url, callback=self.parse, meta={"playwright": True})
 
     def parse(self, response):
         open_appointments = response.css('div.js_dogodekBox.dogodek')
         self.log(open_appointments)
         for appointment in open_appointments:
             free_date = appointment.css(".calendarBox > .sr-only::text").get()
-            location = appointment.css(".upperOpomnikDiv > span:nth-child(2)::text").get()
-            time = appointment.css(".contentOpomnik.celotnaSirina > div:nth-child(6) > span.bold::text").get()
+            location = appointment.css(
+                ".upperOpomnikDiv > span:nth-child(2)::text").get()
+            time = appointment.css(
+                ".contentOpomnik.celotnaSirina > div:nth-child(6) > span.bold::text").get()
             self.log(f"Free date: {free_date}")
             self.log(f"At location: {location}")
             self.log(f"At time: {time}\n")
